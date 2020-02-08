eac3053a2514134853fd274b5903ff323d8bb4ca
Akash Pillai <akashp807@gmail.com>
2020-02-08 09:22:20 -0600

    Add some gradients

--- src/gradients.js
+++ src/gradients.js
@@ -1,8 +1,23 @@
+// uigradients.com
+
 export const gradients = [
     ['#1CD8D2', '#93EDC7'],
     ['#A1FFCE', '#FAFFD1'],
     ['#a3fc9c', '#4ef5ff'],
-    ['#00FFF4', '#4E71D6'],
-    ['#be22ed', '#ff1c97'],
-    ['#f03b3b', '#ffc400']
+    ['#00d2ff', '#3a7bd5'],
+    ['#4776E6', '#8E54E9'],
+    ['#FFEDBC', '#ff1c97'],
+    ['#f857a6', '#ff5858'],
+    ['#FFB88C', '#FFEDBC'],
+    ['#FF8008', '#FFC837'],
+    ['#93F9B9', '#1D976C'],
+    ['#A6FFCB', '#12D8FA'],
+    ['#1FA2FF', '#00CDAC'],
+    ['#E1F5C4', '#EDE574'],
+    ['#FFEEEE', '#fc6767'],
+    ['#fbc7d4', '#ee9ca7'],
+    ['#6fb1fc', '#9796f0'],
+    ['#86A8E7', '#eaafc8'],
+    ['#a8c0ff', '#91EAE4'],
+    ['#FFEEEE', '#DDEFBB'],
 ]
