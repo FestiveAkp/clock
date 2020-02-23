fd54a1250d4a9b9f1e6e514893c02ca4a751e6a3
Akash Pillai <akashp807@gmail.com>
2020-02-22 20:17:32 -0600

    Who needs prerendering anyway

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,4 @@
 module.exports = {
-    pluginOptions: {
-        prerenderSpa: {
-            registry: undefined,
-            renderRoutes: [
-                '/'
-            ],
-            useRenderEvent: true,
-            headless: true,
-            onlyProduction: true
-        }
-    },
-
     assetsDir: 'assets/',
-
     publicPath: '/clock/'
 }
