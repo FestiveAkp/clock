0070aed3cb6b77e845d8678060d68fda0221e8a8
Akash Pillai <akashp807@gmail.com>
2020-03-15 16:40:25 -0500

    Ok fine, let's try prerendering again

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
+    pluginOptions: {
+        prerenderSpa: {
+            registry: undefined,
+            renderRoutes: [
+                '/'
+            ],
+            useRenderEvent: true,
+            headless: true,
+            onlyProduction: true
+        }
+    },
+
     publicPath: '/clock/'
 }
