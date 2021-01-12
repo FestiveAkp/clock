29dc506a695d9c38ed9f2791e0b4727b28ba8dd8
Akash Pillai <akashp807@gmail.com>
2021-01-11 19:16:16 -0600

    Fix favicon paths

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,17 +10,17 @@ export default {
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
             { hid: 'description', name: 'description', content: 'A beautifully simple online date and time display in front of a colorful moving gradient.' },
             { name: 'msapplication-TileColor', content: '#da532c' },
-            { name: 'msapplication-config', content: '/browserconfig.xml' },
+            { name: 'msapplication-config', content: '/clock/browserconfig.xml' },
             { name: 'theme-color', content: '#000000' }
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-            { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-            { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-            { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-            { rel: 'manifest', href: '/site.webmanifest' },
-            { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
-            { rel: 'shortcut icon', href: '/favicon.ico' }
+            { rel: 'icon', type: 'image/x-icon', href: '/clock/favicon.ico' },
+            { rel: 'apple-touch-icon', sizes: '180x180', href: '/clock/apple-touch-icon.png' },
+            { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/clock/favicon-32x32.png' },
+            { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/clock/favicon-16x16.png' },
+            { rel: 'manifest', href: '/clock/site.webmanifest' },
+            { rel: 'mask-icon', href: '/clock/safari-pinned-tab.svg', color: '#5bbad5' },
+            { rel: 'shortcut icon', href: '/clock/favicon.ico' }
         ],
         script: [
             { 'data-goatcounter': 'https://akashdev.goatcounter.com/count', async: true, src: '//gc.zgo.at/count.js', body: true }
