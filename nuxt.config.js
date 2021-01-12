1437589d0d260b43f185736a446cc8386304629f
Akash Pillai <akashp807@gmail.com>
2021-01-11 19:08:48 -0600

    Add favicon, title, and meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,23 @@ export default {
 
     // Global page headers (https://go.nuxtjs.dev/config-head)
     head: {
-        title: 'clock',
+        title: 'Gradient Clock',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' }
+            { hid: 'description', name: 'description', content: 'A beautifully simple online date and time display in front of a colorful moving gradient.' },
+            { name: 'msapplication-TileColor', content: '#da532c' },
+            { name: 'msapplication-config', content: '/browserconfig.xml' },
+            { name: 'theme-color', content: '#000000' }
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+            { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+            { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+            { rel: 'manifest', href: '/site.webmanifest' },
+            { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+            { rel: 'shortcut icon', href: '/favicon.ico' }
         ],
         script: [
             { 'data-goatcounter': 'https://akashdev.goatcounter.com/count', async: true, src: '//gc.zgo.at/count.js', body: true }
