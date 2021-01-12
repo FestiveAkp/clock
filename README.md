7f60e3287f3dc9a1cae31eb4604c60acc2fa214a
Akash Pillai <akashp807@gmail.com>
2021-01-11 18:28:44 -0600

    Rewrite project using Nuxt.js

--- README.md
+++ README.md
@@ -1,24 +1,20 @@
-# nicetime
+# clock
 
-## Project setup
-```
-npm install
-```
+## Build Setup
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+```bash
+# install dependencies
+$ yarn install
 
-### Compiles and minifies for production
-```
-npm run build
-```
+# serve with hot reload at localhost:3000
+$ yarn dev
 
-### Lints and fixes files
-```
-npm run lint
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ components/Clock.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="container is-fluid has-text-centered">
+        <h1 v-text="time" class="time-display"></h1>
+        <h2 v-text="date" class="date-display"></h2>
+    </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+    name: 'Clock',
+
+    data() {
+        return {
+            time: '',
+            date: ''
+        };
+    },
+
+    methods: {
+        getCurrentTime() {
+            this.time = moment().format('h:mm:ss a');
+            this.date = moment().format('dddd, LL');
+        }
+    },
+
+    mounted() {
+        this.getCurrentTime();
+        this.interval = setInterval(this.getCurrentTime, 1000);
+    },
+
+    beforeDestroy() {
+        clearInterval(this.interval);
+    }
+}
+</script>
+
+<style scoped>
+    /* https://google-webfonts-helper.herokuapp.com/fonts */
+    @font-face {
+        font-family: 'Varela Round';
+        font-style: normal;
+        font-weight: 400;
+        src: local('Varela Round Regular'), local('VarelaRound-Regular'),
+            url('~assets/fonts/varela-round-v12-latin-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+            url('~assets/fonts/varela-round-v12-latin-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+    }
+
+    .time-display {
+        font-size: 12vw;
+        font-family: 'Varela Round', sans-serif;
+    }
+
+    .date-display {
+        font-size: 3vw;
+        font-family: 'Varela Round', sans-serif;
+    }
+
+    a {
+        margin-top: 3rem;
+    }
+
+    div.container {
+        margin-bottom: 2em;
+    }
+</style>
