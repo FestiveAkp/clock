7df257c464eee913a5510131f6148bddaf7f93b1
Akash Pillai <akashp807@gmail.com>
2021-01-22 09:28:37 -0600

    Import Chakra UI Vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,9 @@ export default {
     // Modules (https://go.nuxtjs.dev/config-modules)
     modules: [
         // https://go.nuxtjs.dev/bootstrap
-        '@nuxtjs/bulma'
+        '@nuxtjs/bulma',
+        '@chakra-ui/nuxt',
+        '@nuxtjs/emotion'
     ],
 
     // Build Configuration (https://go.nuxtjs.dev/config-build)
