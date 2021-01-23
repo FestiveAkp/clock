b9d43fac7c9c48f5ad38e5703a8b0ddcba1ec31b
Akash Pillai <akashp807@gmail.com>
2021-01-23 02:58:49 -0600

    Fix overridden CSS styles in menu

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,6 +39,7 @@ export default {
 
     // Global CSS (https://go.nuxtjs.dev/config-css)
     css: [
+        '@/assets/app.css'
     ],
 
     // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
