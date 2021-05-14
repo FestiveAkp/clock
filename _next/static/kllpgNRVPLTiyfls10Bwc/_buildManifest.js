830e95345d9fc2582eb26570fc9cf6cd7c78fa26
FestiveAkp <FestiveAkp@users.noreply.github.com>
2021-05-14 22:06:22 +0000

    deploy: ee36e26eaaa9f937091b196610d8923a4bff2e94

---
+++ _next/static/kllpgNRVPLTiyfls10Bwc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-469f627404362b4766be.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-6ed829398fdd724afc6d.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
