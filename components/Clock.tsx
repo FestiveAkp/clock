ed72931ddabd611584a02b9948973d51497dd113
Akash Pillai <akashp807@gmail.com>
2022-05-15 18:05:22 -0500

    Add monospace fix

--- components/Clock.tsx
+++ components/Clock.tsx
@@ -58,15 +58,56 @@ export default function Clock(props: Props) {
         return () => clearInterval(interval)
     }, []);
 
-    const marginBottom = fontSelection.includes('Pacifico') ? 28
-                       : fontSelection.includes('Arima') ? 14
-                       : 20;
+    // Check if string is only numeric digits
+    const isNumeric = (num: string) => num.match(/^[0-9]+$/);
+
+    // Move display up a bit based on the font
+    const getDisplayMarginBottom = (font: string) => {
+        if (font.includes('Pacifico')) {
+            return 28;
+        }
+        else if (font.includes('Arima')) {
+            return 14;
+        }
+        return 20;
+    }
+
+    // Fix the widths of all characters so the display doesn't wiggle
+    const getMonospaceCharacterWidth = (digit: string, font: string) => {
+        if (digit === ' ') {
+            return '3vw';
+        }
+        else if (digit === ':' || !isNumeric(digit)) {
+            return 'auto';
+        }
+        else if (font.includes('Arima')) {
+            return '7.1vw';
+        }
+        else if (font.includes('Pacifico')) {
+            return '6.5vw';
+        }
+        return '7.75vw';
+    }
+
     const dateFontSize = isHidingClock ? '6vw' : '3vw';
+    const clockMarginBottom = fontSelection.includes('Pacifico') ? 8 : 0;
 
     return (
-        <Box as="section" className="clock-face" textAlign="center" fontFamily={fontSelection} cursor="default" mb={marginBottom}>
+        <Box
+            as="section"
+            className="clock-face"
+            textAlign="center"
+            fontFamily={fontSelection}
+            cursor="default"
+            mb={getDisplayMarginBottom(fontSelection)}
+        >
             {!isHidingClock &&
-                <Text fontSize="12vw">{formatTime(time)}</Text>
+                <Box display="flex" flexDirection="row" mb={clockMarginBottom}>
+                    {Array.from(formatTime(time)).map((digit, i) => {
+                        const width = getMonospaceCharacterWidth(digit, fontSelection);
+                        return <Text key={i} fontSize="12vw" width={width}>{digit}</Text>
+                    })}
+                </Box>
             }
             {!isHidingDate &&
                 <Text fontSize={dateFontSize}>{formatDate(date)}</Text>
